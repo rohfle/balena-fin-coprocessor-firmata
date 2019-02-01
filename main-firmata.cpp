0897cf50f9771a97f10d4bcd7bea6998d5907aa5
Alex Bucknall <alex.bucknall@gmail.com>
2019-02-01 00:07:46 +0000

    Temp changes to main whilst bug fixing

--- main-firmata.cpp
+++ main-firmata.cpp
@@ -1,6 +1,6 @@
 #include "balena.h"
 #include "Firmata.h"
-#include "Serial.h"
+//#include "Serial.h"
 
 /*
  * Firmata is a generic protocol for communicating with microcontrollers
@@ -18,7 +18,21 @@
  * This example code is in the public domain.
  */
 
-BalenaClass Balena;
+#define I2C_WRITE                   0x00
+#define I2C_READ                    0x08
+#define I2C_READ_CONTINUOUSLY       0x10
+#define I2C_STOP_READING            0x18
+#define I2C_READ_WRITE_MODE_MASK    0x18
+#define I2C_10BIT_ADDRESS_MODE_MASK 0x20
+#define I2C_END_TX_MASK             0x40
+#define I2C_STOP_TX                 1
+#define I2C_RESTART_TX              0
+#define I2C_MAX_QUERIES             8
+#define I2C_REGISTER_NOT_SPECIFIED  -1
+
+// the minimum interval for sampling analog input
+#define MINIMUM_SAMPLING_INTERVAL 1
+
 SerialClass Serial(1);
 firmata::FirmataClass Firmata;
 
@@ -81,16 +95,16 @@ void reportAnalogCallback(byte analogPin, int value)
 
 /* Add sysex callback implementation */
 
-//void sysexCallback(byte command, byte argc, byte *argv)
-//{
-//  byte mode;
-//  byte stopTX;
-//  byte slaveAddress;
-//  byte data;
-//  int slaveRegister;
-//  unsigned int delayTime;
-//
-//  switch (command) {
+void sysexCallback(byte command, byte argc, byte *argv)
+{
+  byte mode;
+  byte stopTX;
+  byte slaveAddress;
+  byte data;
+  int slaveRegister;
+  unsigned int delayTime;
+
+  switch (command) {
 //    case I2C_REQUEST:
 //      mode = argv[1] & I2C_READ_WRITE_MODE_MASK;
 //      if (argv[1] & I2C_10BIT_ADDRESS_MODE_MASK) {
@@ -234,100 +248,185 @@ void reportAnalogCallback(byte analogPin, int value)
 //        analogWriteCallback(argv[0], val);
 //      }
 //      break;
-//    case CAPABILITY_QUERY:
-//      Firmata.write(START_SYSEX);
-//      Firmata.write(CAPABILITY_RESPONSE);
-//      for (byte pin = 0; pin < TOTAL_PINS; pin++) {
-//        if (IS_PIN_DIGITAL(pin)) {
-//          Firmata.write((byte)INPUT);
-//          Firmata.write(1);
-//          Firmata.write((byte)PIN_MODE_PULLUP);
-//          Firmata.write(1);
-//          Firmata.write((byte)OUTPUT);
-//          Firmata.write(1);
-//        }
-//        if (IS_PIN_ANALOG(pin)) {
-//          Firmata.write(PIN_MODE_ANALOG);
-//          Firmata.write(10); // 10 = 10-bit resolution
-//        }
-//        if (IS_PIN_PWM(pin)) {
-//          Firmata.write(PIN_MODE_PWM);
-//          Firmata.write(DEFAULT_PWM_RESOLUTION);
-//        }
-//        if (IS_PIN_DIGITAL(pin)) {
-//          Firmata.write(PIN_MODE_SERVO);
-//          Firmata.write(14);
-//        }
-//        if (IS_PIN_I2C(pin)) {
-//          Firmata.write(PIN_MODE_I2C);
-//          Firmata.write(1);  // TODO: could assign a number to map to SCL or SDA
-//        }
-//#ifdef FIRMATA_SERIAL_FEATURE
-//        serialFeature.handleCapability(pin);
-//#endif
-//        Firmata.write(127);
-//      }
-//      Firmata.write(END_SYSEX);
-//      break;
-//    case PIN_STATE_QUERY:
-//      if (argc > 0) {
-//        byte pin = argv[0];
-//        Firmata.write(START_SYSEX);
-//        Firmata.write(PIN_STATE_RESPONSE);
-//        Firmata.write(pin);
-//        if (pin < TOTAL_PINS) {
-//          Firmata.write(Firmata.getPinMode(pin));
-//          Firmata.write((byte)Firmata.getPinState(pin) & 0x7F);
-//          if (Firmata.getPinState(pin) & 0xFF80) Firmata.write((byte)(Firmata.getPinState(pin) >> 7) & 0x7F);
-//          if (Firmata.getPinState(pin) & 0xC000) Firmata.write((byte)(Firmata.getPinState(pin) >> 14) & 0x7F);
-//        }
-//        Firmata.write(END_SYSEX);
-//      }
-//      break;
-//    case ANALOG_MAPPING_QUERY:
-//      Firmata.write(START_SYSEX);
-//      Firmata.write(ANALOG_MAPPING_RESPONSE);
-//      for (byte pin = 0; pin < TOTAL_PINS; pin++) {
-//        Firmata.write(IS_PIN_ANALOG(pin) ? PIN_TO_ANALOG(pin) : 127);
-//      }
-//      Firmata.write(END_SYSEX);
-//      break;
-//
-//    case SERIAL_MESSAGE:
-//#ifdef FIRMATA_SERIAL_FEATURE
-//      serialFeature.handleSysex(command, argc, argv);
-//#endif
-//      break;
-//  }
-//}
+    case CAPABILITY_QUERY:
+      Firmata.write(START_SYSEX);
+      Firmata.write(CAPABILITY_RESPONSE);
+      for (byte pin = 0; pin < TOTAL_PINS; pin++) {
+        if (IS_PIN_DIGITAL(pin)) {
+          Firmata.write((byte)PIN_MODE_INPUT);
+          Firmata.write(1);
+          Firmata.write((byte)PIN_MODE_PULLUP);
+          Firmata.write(1);
+          Firmata.write((byte)PIN_MODE_OUTPUT);
+          Firmata.write(1);
+        }
+        if (IS_PIN_ANALOG(pin)) {
+          Firmata.write(PIN_MODE_ANALOG);
+          Firmata.write(10); // 10 = 10-bit resolution
+        }
+        if (IS_PIN_PWM(pin)) {
+          Firmata.write(PIN_MODE_PWM);
+          Firmata.write(DEFAULT_PWM_RESOLUTION);
+        }
+        if (IS_PIN_DIGITAL(pin)) {
+          Firmata.write(PIN_MODE_SERVO);
+          Firmata.write(14);
+        }
+        if (IS_PIN_I2C(pin)) {
+          Firmata.write(PIN_MODE_I2C);
+          Firmata.write(1);  // TODO: could assign a number to map to SCL or SDA
+        }
+#ifdef FIRMATA_SERIAL_FEATURE
+        serialFeature.handleCapability(pin);
+#endif
+        Firmata.write(127);
+      }
+      Firmata.write(END_SYSEX);
+      break;
+    case PIN_STATE_QUERY:
+      if (argc > 0) {
+        byte pin = argv[0];
+        Firmata.write(START_SYSEX);
+        Firmata.write(PIN_STATE_RESPONSE);
+        Firmata.write(pin);
+        if (pin < TOTAL_PINS) {
+          Firmata.write(Firmata.getPinMode(pin));
+          Firmata.write((byte)Firmata.getPinState(pin) & 0x7F);
+          if (Firmata.getPinState(pin) & 0xFF80) Firmata.write((byte)(Firmata.getPinState(pin) >> 7) & 0x7F);
+          if (Firmata.getPinState(pin) & 0xC000) Firmata.write((byte)(Firmata.getPinState(pin) >> 14) & 0x7F);
+        }
+        Firmata.write(END_SYSEX);
+      }
+      break;
+    case ANALOG_MAPPING_QUERY:
+      Firmata.write(START_SYSEX);
+      Firmata.write(ANALOG_MAPPING_RESPONSE);
+      for (byte pin = 0; pin < TOTAL_PINS; pin++) {
+        Firmata.write(IS_PIN_ANALOG(pin) ? PIN_TO_ANALOG(pin) : 127);
+      }
+      Firmata.write(END_SYSEX);
+      break;
 
-int main(void)
+    case SERIAL_MESSAGE:
+#ifdef FIRMATA_SERIAL_FEATURE
+      serialFeature.handleSysex(command, argc, argv);
+#endif
+      break;
+  }
+}
+
+int main_test(void)
 {
-
+	long baud = 115200;
+	int i = 0;
+		  unsigned char welcome_string[] = "Silicon Labs UART Code test!\r\f";
 	Firmata.setFirmwareVersion(FIRMATA_FIRMWARE_MAJOR_VERSION, FIRMATA_FIRMWARE_MINOR_VERSION);
 	Firmata.attach(DIGITAL_MESSAGE, digitalWriteCallback);
+	Firmata.attach(START_SYSEX, sysexCallback);
 	Firmata.attach(SET_PIN_MODE, setPinModeCallback);
-	Firmata.begin(57600);
+//////	Firmata.begin(baud);
 
-	Serial.begin(115200);
+	Serial.begin(baud);
+		Firmata.begin(Serial);
 
-	Firmata.begin(Serial);
+//	  // Print welcome message
+//	  for (i = 0 ; welcome_string[i] != 0; i++)
+//	  {
+//	    tx_buffer[i] = welcome_string[i];
+//	  }
+//	  USART_IntSet(USART0, USART_IFS_TXC);
+//
+//	  while (1)
+//	  {
+//	    /* When notified by the RX handler, copy data from the RX buffer to the
+//	     * TX buffer, and start the TX handler */
+//	    if (1)
+//	    {
+//
+//	      USART_IntDisable(USART0, USART_IEN_RXDATAV);
+//	      USART_IntDisable(USART0, USART_IEN_TXC);
+//			digitalWrite(3,0);
+//
+//	      for (i = 0; rx_buffer[i] != 0 && i < BUFFER_SIZE-3; i++)
+//	      {
+//	        tx_buffer[i] = rx_buffer[i];
+//	      }
+//	      tx_buffer[i++] = (char)223;
+//	      tx_buffer[i++] = '\r';
+//	      tx_buffer[i++] = '\f';
+//	      tx_buffer[i] = '\0';
+//	      rx_data_ready = 0;
+//
+//	      USART_IntEnable(USART0, USART_IEN_RXDATAV);
+//	      USART_IntEnable(USART0, USART_IEN_TXC);
+//	      USART_IntSet(USART0, USART_IFS_TXC);
+//	    }
+//	}
+//
+//	while(1){
+////		Serial.readBytes(data, 12);
+////		Serial.write(data);
+//	}
+//	Firmata.begin(Serial);
 
-	unsigned char test [64]= "test\n";
-	Serial.write(test);
+
 
 
 	  while (1) {
+
 		  byte i;
 
-		   for (i = 0; i < TOTAL_PORTS; i++) {
-		     outputPort(i, readPort(i, 0xff));
-		   }
+//		   for (i = 0; i < TOTAL_PORTS; i++) {
+//		     outputPort(i, readPort(i, 0xff));
+//		   }
 
-		   while (Firmata.available()) {
+		   while (Firmata.available() != 0) {
+				digitalWrite(3,0);
 		     Firmata.processInput();
 		   }
+			digitalWrite(3,1);
+
 	  };
+
+//	 USART_InitAsync_TypeDef init = USART_INITASYNC_DEFAULT;
+//	  unsigned char welcome_string[] = "Silicon Labs UoART Code example!\r\f";
+//	  uint32_t i;
+//	  long baud = 115200;
+//	  	Serial.begin(baud);
+//
+//	  	Serial.write(welcome_string);
+
+
+	  // Chip errata
+//	  CHIP_Init();
+
+//	  // Enable oscillator to GPIO and USART0 modules
+//	  CMU_ClockEnable(cmuClock_GPIO, true);
+//	  CMU_ClockEnable(cmuClock_USART0, true);
+//
+//	  // set pin modes for USART TX and RX pins
+//	  GPIO_PinModeSet(gpioPortA, 1, gpioModeInput, 0);
+//	  GPIO_PinModeSet(gpioPortA, 0, gpioModePushPull, 1);
+//
+//	  // Initialize USART asynchronous mode and route pins
+//	  USART_InitAsync(USART0, &init);
+//	  USART0->ROUTEPEN |= USART_ROUTEPEN_TXPEN | USART_ROUTEPEN_RXPEN;
+//	  USART0->ROUTELOC0 = USART_ROUTELOC0_RXLOC_LOC0 | USART_ROUTELOC0_TXLOC_LOC0;
+//
+//	  //Initializae USART Interrupts
+//	  USART_IntEnable(USART0, USART_IEN_RXDATAV);
+//	  USART_IntEnable(USART0, USART_IEN_TXC);
+//
+//	  //Enabling USART Interrupts
+//	  NVIC_EnableIRQ(USART0_RX_IRQn);
+//	  NVIC_EnableIRQ(USART0_TX_IRQn);
+
+	  // Print welcome message
+//	  for (i = 0 ; welcome_string[i] != 0; i++)
+//	  {
+//	    tx_buffer[i] = welcome_string[i];
+//	  }
+//	  USART_IntSet(USART0, USART_IFS_TXC);
 
   return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,50 +1,278 @@
-#include "balena.h"
-#include "Firmata.h"
-#include "Serial.h"
-
-int main_test(void)
-{
-	BalenaClass Balena;
-//	firmata::FirmataClass Firmata;
-
-	SerialClass Serial(1,115200);
-
-	unsigned char buf[32] = "12345678910111213141517181920\r\n";
-
-	Serial.begin(115200);
-
-	Serial.write(buf);
-
-//	Firmata.begin(9600);
-
-	while(1){
-			for(volatile long i=0; i<300000; i++);
-		        // Turn on the LED
-	    		digitalWrite(3,1);
-
-		        // Add some delay
-//		        for(volatile long i=0; i<300000; i++)
-//		            ;
-		    	Serial.write(buf);
-
-		    	delay(1000);
-
-		        // Turn off the LED
-		    	digitalWrite(3,0);
-
-		        // Add some more delay
-
-		    	delay(1000);
-
-//		        for(volatile long i=0; i<300000; i++)
-//		            ;
-	};
-
-
-
-  while (1) {
-	  // Do Firmata things
-  };
-
-  return 0;
-}
+//////
+//////#include "em_device.h"
+//////#include "em_chip.h"
+//////#include "init_mcu.h"
+//////#include "init_board.h"
+//////#include "init_app.h"
+//////#include "uartdrv.h"
+//////#include "hal-config-board.h"
+//////#include "uartdrv_config.h"
+//////#include "ble-configuration.h"
+//////#include "board_features.h"
+//////
+///////* Bluetooth stack headers */
+//////#include "bg_types.h"
+//////#include "native_gecko.h"
+//////#include "gatt_db.h"
+//////
+//////#ifndef MAX_CONNECTIONS
+//////#define MAX_CONNECTIONS 4
+//////#endif
+//////uint8_t bluetooth_stack_heap[DEFAULT_BLUETOOTH_HEAP(MAX_CONNECTIONS)];
+//////
+//////#define EMDRV_UARTDRV_HW_FLOW_CONTROL_ENABLE 0
+//////
+//////UARTDRV_HandleData_t uarthandleData;
+//////UARTDRV_Handle_t uartHandle = &uarthandleData;
+//////DEFINE_BUF_QUEUE(EMDRV_UARTDRV_MAX_CONCURRENT_RX_BUFS, rxBufferQueue);
+//////DEFINE_BUF_QUEUE(EMDRV_UARTDRV_MAX_CONCURRENT_TX_BUFS, txBufferQueue);
+//////uint8_t buffer[64] = "noob sauce\r\n";
+//////
+//////#define MY_UART                                     \
+//////{                                                   \
+//////  USART0,                                           \
+//////  115200,                                           \
+//////  _USART_ROUTELOC0_TXLOC_LOC0,                     \
+//////  _USART_ROUTELOC0_RXLOC_LOC0,                     \
+//////  usartStopbits1,                                   \
+//////  usartNoParity,                                    \
+//////  usartOVS16,                                       \
+//////  false,                                            \
+//////  uartdrvFlowControlNone,							    \
+//////  gpioPortA,                                        \
+//////  4,                                                \
+//////  gpioPortA,                                        \
+//////  5,                                                \
+//////  (UARTDRV_Buffer_FifoQueue_t *)&rxBufferQueue,     \
+//////  (UARTDRV_Buffer_FifoQueue_t *)&txBufferQueue,     \
+//////}
+////////  _USART_ROUTELOC1_CTSLOC_LOC0,                    \
+////////  _USART_ROUTELOC1_RTSLOC_LOC0                     \
+//////}
+//////
+//////static const gecko_configuration_t config = {
+//////  .config_flags = 0,
+//////  .sleep.flags = 0,
+//////  .bluetooth.max_connections = MAX_CONNECTIONS,
+//////  .bluetooth.heap = bluetooth_stack_heap,
+//////  .bluetooth.heap_size = sizeof(bluetooth_stack_heap),
+//////  .bluetooth.sleep_clock_accuracy = 100, // ppm
+//////  .gattdb = &bg_gattdb_data,
+//////  .ota.flags = 0,
+//////  .ota.device_name_len = 3,
+//////  .ota.device_name_ptr = "OTA",
+//////#if (HAL_PA_ENABLE) && defined(FEATURE_PA_HIGH_POWER)
+//////  .pa.config_enable = 1, // Enable high power PA
+//////  .pa.input = GECKO_RADIO_PA_INPUT_VBAT, // Configure PA input to VBAT
+//////#endif // (HAL_PA_ENABLE) && defined(FEATURE_PA_HIGH_POWER)
+//////};
+//////
+////////  uartdrvFlowControlHwUart
+//////
+//////void setup_uartdrv(void) {                                      //Setup uartdrv
+//////		GPIO_PinOutClear(BSP_VCOM_ENABLE_PORT, BSP_VCOM_ENABLE_PIN);
+//////      //Configuration for USART0, Location 1
+//////      UARTDRV_Init_t uartInitData = MY_UART;
+//////      UARTDRV_Init(uartHandle, &uartInitData);
+//////
+//////}
+//////
+//////void callback(UARTDRV_Handle_t handle,
+//////              Ecode_t transferStatus,
+//////              uint8_t *data,
+//////              UARTDRV_Count_t transferCount)
+//////{
+//////  (void)handle;
+//////  (void)transferStatus;
+//////  (void)data;
+//////  (void)transferCount;
+//////	for(volatile long i=0; i<3; i++){
+//////		GPIO_PinOutSet(gpioPortF, 6U);
+//////		for(volatile long i=0; i<3000000; i++);
+//////		GPIO_PinOutClear(gpioPortF, 6U);
+//////		for(volatile long i=0; i<3000000; i++);
+//////	};
+//////}
+//////
+//////int main(void)
+//////{
+//////    CHIP_Init();
+//////
+//////    CMU_ClockEnable(cmuClock_HFPER, true);
+//////    CMU_ClockEnable(cmuClock_USART0, true);
+//////    CMU_ClockEnable(cmuClock_GPIO, true);
+//////	  // Initialize device
+//////	  initMcu();
+//////	  // Initialize board
+//////	  initBoard();
+//////	  // Initialize application
+//////	  initApp();
+//////
+//////	   gecko_init(&config);
+//////
+//////	setup_uartdrv();
+//////
+//////	GPIO_PinModeSet(gpioPortF, 6U, gpioModePushPull, 0);
+//////
+//////	for(volatile long i=0; i<3; i++){
+//////		GPIO_PinOutSet(gpioPortF, 6U);
+//////		for(volatile long i=0; i<3000000; i++);
+//////		GPIO_PinOutClear(gpioPortF, 6U);
+//////		for(volatile long i=0; i<3000000; i++);
+//////	};
+//////
+//////    // Transmit data using a non-blocking transmit function
+////////    UARTDRV_Transmit(uartHandle, buffer, 64, callback);
+//////    UARTDRV_Receive(uartHandle, buffer, 64, callback);
+//////
+//////  while (1) {
+//////
+//////		// Add some delay
+//////
+////////		UARTDRV_ForceTransmit(uartHandle, buffer, 64);
+////////		for(volatile long i=0; i<3000000; i++);
+//////  }
+//////}
+////
+//
+//
+//#include "em_device.h"
+//#include "em_cmu.h"
+//#include "em_gpio.h"
+////#include "em_usart.h"
+//#include "em_chip.h"
+//#include "Serial.h"
+//
+////#define BUFFER_SIZE 80
+////volatile uint32_t rx_data_ready = 0;
+////volatile char rx_buffer[BUFFER_SIZE];
+////volatile char tx_buffer[BUFFER_SIZE];
+//
+///**************************************************************************//**
+// * @brief USART0 RX interrupt service routine
+// *****************************************************************************/
+////void USART0_RX_IRQHandler(void)
+////{
+////  static uint32_t i = 0;
+////  uint32_t flags;
+////  flags = USART_IntGet(USART0);
+////  USART_IntClear(USART0, flags);
+////
+////  /* Store incoming data into rx_buffer, set rx_data_ready when a full
+////  * line has been received
+////  */
+////  rx_buffer[i++] = USART_Rx(USART0);;
+////
+////  if (rx_buffer[i - 1] == '\r' || rx_buffer[i - 1] == '\f')
+////  {
+////    rx_data_ready = 1;
+////    rx_buffer[i - 1] = '\0'; // Overwrite CR or LF character
+////    i = 0;
+////  }
+////
+////  if ( i >= BUFFER_SIZE - 2 )
+////  {
+////    rx_data_ready = 1;
+////    rx_buffer[i] = '\0'; // Do not overwrite last character
+////    i = 0;
+////  }
+////}
+////
+/////**************************************************************************//**
+//// * @brief USART0 TX interrupt service routine
+//// *****************************************************************************/
+////void USART0_TX_IRQHandler(void)
+////{
+////  static uint32_t i = 0;
+////  uint32_t flags;
+////  flags = USART_IntGet(USART0);
+////  USART_IntClear(USART0, flags);
+////
+////  if (flags & USART_IF_TXC)
+////  {
+////    if (i < BUFFER_SIZE && tx_buffer[i] != '\0')
+////    {
+////    	USART_Tx(USART0, tx_buffer[i++]);; // Transmit byte
+////    }
+////    else
+////    {
+////      i = 0; // No more data to send
+////    }
+////  }
+////}
+//
+///**************************************************************************//**
+// * @brief Main function
+// *****************************************************************************/
+//int main_void(void)
+//{
+////  USART_InitAsync_TypeDef init = USART_INITASYNC_DEFAULT;
+//  char welcome_string[] = "Silicon Labs TEST Code example!\r\f";
+//  uint32_t i;
+////
+////  // Chip errata
+////  CHIP_Init();
+////
+////  // Enable oscillator to GPIO and USART0 modules
+////  CMU_ClockEnable(cmuClock_GPIO, true);
+////  CMU_ClockEnable(cmuClock_USART0, true);
+////
+////  // set pin modes for USART TX and RX pins
+////  GPIO_PinModeSet(gpioPortA, 1, gpioModeInput, 0);
+////  GPIO_PinModeSet(gpioPortA, 0, gpioModePushPull, 1);
+////
+////  // Initialize USART asynchronous mode and route pins
+////  USART_InitAsync(USART0, &init);
+////  USART0->ROUTEPEN |= USART_ROUTEPEN_TXPEN | USART_ROUTEPEN_RXPEN;
+////  USART0->ROUTELOC0 = USART_ROUTELOC0_RXLOC_LOC0 | USART_ROUTELOC0_TXLOC_LOC0;
+////
+////  //Initializae USART Interrupts
+////  USART_IntEnable(USART0, USART_IEN_RXDATAV);
+////  USART_IntEnable(USART0, USART_IEN_TXC);
+////
+////  //Enabling USART Interrupts
+////  NVIC_EnableIRQ(USART0_RX_IRQn);
+////  NVIC_EnableIRQ(USART0_TX_IRQn);
+//
+//  SerialClass Serial(1);
+//
+//  Serial.begin(115200);
+//
+//  // Print welcome message
+//  for (i = 0 ; welcome_string[i] != 0; i++)
+//  {
+//    tx_buffer[i] = welcome_string[i];
+//  }
+//  USART_IntSet(USART0, USART_IFS_TXC);
+//
+//  while (1)
+//  {
+//    /* When notified by the RX handler, copy data from the RX buffer to the
+//     * TX buffer, and start the TX handler */
+//    if (Serial.available())
+//    {
+//      USART_IntDisable(USART0, USART_IEN_RXDATAV);
+//      USART_IntDisable(USART0, USART_IEN_TXC);
+//
+//      for (i = 0; rx_buffer[i] != 0 && i < BUFFER_SIZE-3; i++)
+//      {
+//        tx_buffer[i] = rx_buffer[i];
+//      }
+//
+//      for (i = 0; rx_buffer[i] != 0 && i < BUFFER_SIZE-3; i++)
+//      {
+//        tx_buffer[i] = rx_buffer[i];
+//      }
+//
+//
+//      tx_buffer[i++] = '\r';
+//      tx_buffer[i++] = '\f';
+//      tx_buffer[i] = '\0';
+//      rx_data_ready = 0;
+//
+//      USART_IntEnable(USART0, USART_IEN_RXDATAV);
+//      USART_IntEnable(USART0, USART_IEN_TXC);
+//      USART_IntSet(USART0, USART_IFS_TXC);
+//    }
+//  }
+//}
