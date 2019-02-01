35ef2d65fef3f79f2348e7a2e092a30732ca4e34
Alex Bucknall <alex.bucknall@gmail.com>
2019-02-01 00:11:03 +0000

    Updated Makefiles

--- GNU ARM v7.2.1 - Default/subdir.mk
+++ GNU ARM v7.2.1 - Default/subdir.mk
@@ -4,8 +4,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../main-firmata.cpp \
-../main.cpp 
+../main-test.cpp 
 
 C_SRCS += \
 ../application_properties.c \
@@ -19,8 +18,7 @@ OBJS += \
 ./init_app.o \
 ./init_board_efr32xg1.o \
 ./init_mcu_efr32xg1.o \
-./main-firmata.o \
-./main.o \
+./main-test.o \
 ./pti.o 
 
 C_DEPS += \
@@ -31,8 +29,7 @@ C_DEPS += \
 ./pti.d 
 
 CPP_DEPS += \
-./main-firmata.d \
-./main.d 
+./main-test.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -64,17 +61,10 @@ init_mcu_efr32xg1.o: ../init_mcu_efr32xg1.c
 	@echo 'Finished building: $<'
 	@echo ' '
 
-main-firmata.o: ../main-firmata.cpp
+main-test.o: ../main-test.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU ARM C++ Compiler'
-	arm-none-eabi-g++ -g -gdwarf-2 -mcpu=cortex-m4 -mthumb '-DEFR32BG1B232F256GM48=1' '-D__StackLimit=0x20000000' '-D__HEAP_SIZE=0xD00' '-DHAL_CONFIG=1' '-D__STACK_SIZE=0x800' -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/Device/SiliconLabs/EFR32BG1B/Include" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/CMSIS/Include" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emlib/src" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emlib/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/hardware/kit/common/drivers" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/hardware/kit/common/bsp" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/hardware/kit/common/halconfig" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/uartdrv/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/bootloader/api" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/bluetooth/ble_stack/inc/common" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/radio/rail_lib/chip/efr32/efr32xg1x" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/hardware/kit/EFR32BG1_BRD4300C/config" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/gpiointerrupt/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/tempdrv/src" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/sleep/src" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/bluetooth/ble_stack/inc/soc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/sleep/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/app/bluetooth/common/util" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/Device/SiliconLabs/EFR32BG1B/Source" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/radio/rail_lib/common" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/Device/SiliconLabs/EFR32BG1B/Source/GCC" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/tempdrv/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/halconfig/inc/hal-config" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/common/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/bootloader" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/balena" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/firmata" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/serial" -O2 -Wall -c -fmessage-length=0 -ffunction-sections -fdata-sections -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -MMD -MP -MF"main-firmata.d" -MT"main-firmata.o" -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
-main.o: ../main.cpp
-	@echo 'Building file: $<'
-	@echo 'Invoking: GNU ARM C++ Compiler'
-	arm-none-eabi-g++ -g -gdwarf-2 -mcpu=cortex-m4 -mthumb '-DEFR32BG1B232F256GM48=1' '-D__StackLimit=0x20000000' '-D__HEAP_SIZE=0xD00' '-DHAL_CONFIG=1' '-D__STACK_SIZE=0x800' -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/Device/SiliconLabs/EFR32BG1B/Include" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/CMSIS/Include" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emlib/src" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emlib/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/hardware/kit/common/drivers" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/hardware/kit/common/bsp" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/hardware/kit/common/halconfig" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/uartdrv/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/bootloader/api" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/bluetooth/ble_stack/inc/common" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/radio/rail_lib/chip/efr32/efr32xg1x" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/hardware/kit/EFR32BG1_BRD4300C/config" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/gpiointerrupt/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/tempdrv/src" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/sleep/src" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/bluetooth/ble_stack/inc/soc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/sleep/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/app/bluetooth/common/util" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/Device/SiliconLabs/EFR32BG1B/Source" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/radio/rail_lib/common" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/Device/SiliconLabs/EFR32BG1B/Source/GCC" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/tempdrv/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/halconfig/inc/hal-config" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/common/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/bootloader" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/balena" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/firmata" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/serial" -O2 -Wall -c -fmessage-length=0 -ffunction-sections -fdata-sections -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -MMD -MP -MF"main.d" -MT"main.o" -o "$@" "$<"
+	arm-none-eabi-g++ -g -gdwarf-2 -mcpu=cortex-m4 -mthumb '-DEFR32BG1B232F256GM48=1' '-D__StackLimit=0x20000000' '-D__HEAP_SIZE=0xD00' '-DHAL_CONFIG=1' '-D__STACK_SIZE=0x800' -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/Device/SiliconLabs/EFR32BG1B/Include" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/CMSIS/Include" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emlib/src" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emlib/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/hardware/kit/common/drivers" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/hardware/kit/common/bsp" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/hardware/kit/common/halconfig" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/uartdrv/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/bootloader/api" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/bluetooth/ble_stack/inc/common" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/radio/rail_lib/chip/efr32/efr32xg1x" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/hardware/kit/EFR32BG1_BRD4300C/config" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/gpiointerrupt/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/tempdrv/src" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/sleep/src" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/bluetooth/ble_stack/inc/soc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/sleep/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/app/bluetooth/common/util" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/Device/SiliconLabs/EFR32BG1B/Source" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/radio/rail_lib/common" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/Device/SiliconLabs/EFR32BG1B/Source/GCC" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/tempdrv/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/halconfig/inc/hal-config" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/emdrv/common/inc" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/platform/bootloader" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/balena" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/firmata" -I"/home/alex/SimplicityStudio/v4_workspace/balena-firmata/protocol/serial" -O2 -Wall -c -fmessage-length=0 -ffunction-sections -fdata-sections -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -MMD -MP -MF"main-test.d" -MT"main-test.o" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
