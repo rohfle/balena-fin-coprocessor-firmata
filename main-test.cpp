2e8683830c6f6071a5f6e80ab4bd7a435c203485
Alex Bucknall <alex.bucknall@gmail.com>
2019-02-03 00:00:40 +0000

    WIP Firmata App Firmware

--- main-test.cpp
+++ main-test.cpp
@@ -1,9 +1,12 @@
-#include "Serial.h"
 #include "balena.h"
-#include <Firmata.h>
+#include "Firmata.h"
+#include "Serial.h"
 
+
+SerialClass Serial;
+//FirmataClass Firmata;
 /* Declare some strings */
-const char     welcomeString[]  = "EFM32 RS-232 - Please press a key\n";
+const char     welcomeString[]  = "balenaFin Firmata Test...\r\n";
 const uint32_t welLen           = sizeof(welcomeString) - 1;
 
 /* Declare a circular buffer structure to use for Rx and Tx queues */
@@ -70,6 +73,171 @@ bool isResetting = false;
 void setPinModeCallback(byte, int);
 void reportAnalogCallback(byte analogPin, int value);
 void sysexCallback(byte, byte, byte*);
+
+/******************************************************************************
+ * @brief  Firmata Functions
+ *
+ *****************************************************************************/
+
+void outputPort(byte portNumber, byte portValue, byte forceSend)
+{
+  // pins not configured as INPUT are cleared to zeros
+  portValue = portValue & portConfigInputs[portNumber];
+  // only send if the value is different than previously sent
+  if (forceSend || previousPINs[portNumber] != portValue) {
+    Firmata.sendDigitalPort(portNumber, portValue);
+    previousPINs[portNumber] = portValue;
+  }
+}
+
+void setPinModeCallback(byte pin, int mode)
+{
+  if (Firmata.getPinMode(pin) == PIN_MODE_IGNORE)
+    return;
+
+//  if (Firmata.getPinMode(pin) == PIN_MODE_I2C && isI2CEnabled && mode != PIN_MODE_I2C) {
+//    // disable i2c so pins can be used for other functions
+//    // the following if statements should reconfigure the pins properly
+//    disableI2CPins();
+//  }
+//  if (IS_PIN_DIGITAL(pin) && mode != PIN_MODE_SERVO) {
+//    if (servoPinMap[pin] < MAX_SERVOS && servos[servoPinMap[pin]].attached()) {
+//      detachServo(pin);
+//    }
+//  }
+  if (IS_PIN_ANALOG(pin)) {
+    reportAnalogCallback(PIN_TO_ANALOG(pin), mode == PIN_MODE_ANALOG ? 1 : 0); // turn on/off reporting
+  }
+  if (IS_PIN_DIGITAL(pin)) {
+    if (mode == INPUT || mode == PIN_MODE_PULLUP) {
+      portConfigInputs[pin / 8] |= (1 << (pin & 7));
+    } else {
+      portConfigInputs[pin / 8] &= ~(1 << (pin & 7));
+    }
+  }
+  Firmata.setPinState(pin, 0);
+  switch (mode) {
+    case PIN_MODE_ANALOG:
+      if (IS_PIN_ANALOG(pin)) {
+        if (IS_PIN_DIGITAL(pin)) {
+          pinMode(PIN_TO_DIGITAL(pin), INPUT);    // disable output driver
+#if ARDUINO <= 100
+          // deprecated since Arduino 1.0.1 - TODO: drop support in Firmata 2.6
+          digitalWrite(PIN_TO_DIGITAL(pin), LOW); // disable internal pull-ups
+#endif
+        }
+        Firmata.setPinMode(pin, PIN_MODE_ANALOG);
+      }
+      break;
+    case INPUT:
+      if (IS_PIN_DIGITAL(pin)) {
+        pinMode(PIN_TO_DIGITAL(pin), INPUT);    // disable output driver
+#if ARDUINO <= 100
+        // deprecated since Arduino 1.0.1 - TODO: drop support in Firmata 2.6
+        digitalWrite(PIN_TO_DIGITAL(pin), LOW); // disable internal pull-ups
+#endif
+        Firmata.setPinMode(pin, INPUT);
+      }
+      break;
+    case PIN_MODE_PULLUP:
+      if (IS_PIN_DIGITAL(pin)) {
+        pinMode(PIN_TO_DIGITAL(pin), INPUT_PULLUP);
+        Firmata.setPinMode(pin, PIN_MODE_PULLUP);
+        Firmata.setPinState(pin, 1);
+      }
+      break;
+    case OUTPUT:
+      if (IS_PIN_DIGITAL(pin)) {
+        if (Firmata.getPinMode(pin) == PIN_MODE_PWM) {
+          // Disable PWM if pin mode was previously set to PWM.
+          digitalWrite(PIN_TO_DIGITAL(pin), LOW);
+        }
+        pinMode(PIN_TO_DIGITAL(pin), OUTPUT);
+        Firmata.setPinMode(pin, OUTPUT);
+      }
+      break;
+    case PIN_MODE_PWM:
+      if (IS_PIN_PWM(pin)) {
+        pinMode(PIN_TO_PWM(pin), OUTPUT);
+        analogWrite(PIN_TO_PWM(pin), 0);
+        Firmata.setPinMode(pin, PIN_MODE_PWM);
+      }
+      break;
+//    case PIN_MODE_SERVO:
+//      if (IS_PIN_DIGITAL(pin)) {
+//        Firmata.setPinMode(pin, PIN_MODE_SERVO);
+//        if (servoPinMap[pin] == 255 || !servos[servoPinMap[pin]].attached()) {
+//          // pass -1 for min and max pulse values to use default values set
+//          // by Servo library
+//          attachServo(pin, -1, -1);
+//        }
+//      }
+//      break;
+//    case PIN_MODE_I2C:
+//      if (IS_PIN_I2C(pin)) {
+//        // mark the pin as i2c
+//        // the user must call I2C_CONFIG to enable I2C for a device
+//        Firmata.setPinMode(pin, PIN_MODE_I2C);
+//      }
+//      break;
+    case PIN_MODE_SERIAL:
+#ifdef FIRMATA_SERIAL_FEATURE
+      serialFeature.handlePinMode(pin, PIN_MODE_SERIAL);
+#endif
+      break;
+    default:
+      Firmata.sendString("Unknown pin mode"); // TODO: put error msgs in EEPROM
+  }
+  // TODO: save status to EEPROM here, if changed
+}
+
+void setPinValueCallback(byte pin, int value)
+{
+  if (pin < TOTAL_PINS && IS_PIN_DIGITAL(pin)) {
+    if (Firmata.getPinMode(pin) == OUTPUT) {
+      Firmata.setPinState(pin, value);
+      digitalWrite(PIN_TO_DIGITAL(pin), value);
+    }
+  }
+}
+
+void reportAnalogCallback(byte analogPin, int value)
+{
+	digitalWrite(3,0);
+  if (analogPin < TOTAL_ANALOG_PINS) {
+    if (value == 0) {
+      analogInputsToReport = analogInputsToReport & ~ (1 << analogPin);
+    } else {
+      analogInputsToReport = analogInputsToReport | (1 << analogPin);
+      // prevent during system reset or all analog pin values will be reported
+      // which may report noise for unconnected analog pins
+      if (!isResetting) {
+        // Send pin value immediately. This is helpful when connected via
+        // ethernet, wi-fi or bluetooth so pin states can be known upon
+        // reconnecting.
+        Firmata.sendAnalog(analogPin, analogRead(analogPin));
+      }
+    }
+  }
+  // TODO: save status to EEPROM here, if changed
+}
+
+void reportDigitalCallback(byte port, int value)
+{
+  if (port < TOTAL_PORTS) {
+    reportPINs[port] = (byte)value;
+    // Send port value immediately. This is helpful when connected via
+    // ethernet, wi-fi or bluetooth so pin states can be known upon
+    // reconnecting.
+    if (value) outputPort(port, readPort(port, portConfigInputs[port]), true);
+  }
+  // do not disable analog reporting on these 8 pins, to allow some
+  // pins used for digital, others analog.  Instead, allow both types
+  // of reporting to be enabled, but check if the pin is configured
+  // as analog when sampling the analog inputs.  Likewise, while
+  // scanning digital pins, portConfigInputs will mask off values from any
+  // pins configured as analog
+}
 
 /******************************************************************************
  * @brief  Sysex Commands
@@ -255,7 +423,7 @@ void sysexCallback(byte command, byte argc, byte *argv)
         }
         if (IS_PIN_I2C(pin)) {
           Firmata.write(PIN_MODE_I2C);
-          Firmata.write(1);  // TODO: could assign a number to map to SCL or SDA
+          Firmata.write(1);
         }
 #ifdef FIRMATA_SERIAL_FEATURE
         serialFeature.handleCapability(pin);
@@ -296,31 +464,94 @@ void sysexCallback(byte command, byte argc, byte *argv)
   }
 }
 
+void systemResetCallback()
+{
+  isResetting = true;
+
+  // initialize a defalt state
+  // TODO: option to load config from EEPROM instead of default
+
+#ifdef FIRMATA_SERIAL_FEATURE
+  serialFeature.reset();
+#endif
+
+//  if (isI2CEnabled) {
+//    disableI2CPins();
+//  }
+
+  for (byte i = 0; i < TOTAL_PORTS; i++) {
+    reportPINs[i] = false;    // by default, reporting off
+    portConfigInputs[i] = 0;  // until activated
+    previousPINs[i] = 0;
+  }
+
+  for (byte i = 0; i < TOTAL_PINS; i++) {
+    // pins with analog capability default to analog input
+    // otherwise, pins default to digital output
+    if (IS_PIN_ANALOG(i)) {
+      // turns off pullup, configures everything
+      setPinModeCallback(i, PIN_MODE_ANALOG);
+    } else if (IS_PIN_DIGITAL(i)) {
+      // sets the output to 0, configures portConfigInputs
+      setPinModeCallback(i, OUTPUT);
+    }
+
+//    servoPinMap[i] = 255;
+  }
+  // by default, do not report any analog inputs
+  analogInputsToReport = 0;
+
+//  detachedServoCount = 0;
+//  servoCount = 0;
+
+  /* send digital inputs to set the initial state on the host computer,
+   * since once in the loop(), this firmware will only send on change */
+  /*
+  TODO: this can never execute, since no pins default to digital input
+        but it will be needed when/if we support EEPROM stored config
+  for (byte i=0; i < TOTAL_PORTS; i++) {
+    outputPort(i, readPort(i, portConfigInputs[i]), true);
+  }
+  */
+  isResetting = false;
+}
+
 /******************************************************************************
  * @brief  Main function
  *
  *****************************************************************************/
 int main(void)
 {
-
-	SerialClass Serial;
-	firmata::FirmataClass Firmata;
 	Firmata.setFirmwareVersion(FIRMATA_FIRMWARE_MAJOR_VERSION, FIRMATA_FIRMWARE_MINOR_VERSION);
 //	Firmata.attach(ANALOG_MESSAGE, analogWriteCallback);
 //	Firmata.attach(DIGITAL_MESSAGE, digitalWriteCallback);
-//	Firmata.attach(REPORT_ANALOG, reportAnalogCallback);
-//	Firmata.attach(REPORT_DIGITAL, reportDigitalCallback);
-//	Firmata.attach(SET_PIN_MODE, setPinModeCallback);
+	Firmata.attach(REPORT_ANALOG, reportAnalogCallback);
+	Firmata.attach(REPORT_DIGITAL, reportDigitalCallback);
+	Firmata.attach(SET_PIN_MODE, setPinModeCallback);
 //	Firmata.attach(SET_DIGITAL_PIN_VALUE, setPinValueCallback);
 	Firmata.attach(START_SYSEX, sysexCallback);
-//	Firmata.attach(SYSTEM_RESET, systemResetCallback);
+	Firmata.attach(SYSTEM_RESET, systemResetCallback);
 
-	Serial.begin(115200);
+	balenaInit();
+
+	digitalWrite(3,1);
+
+	Serial.begin(57600);;
+
 	Firmata.begin(Serial);
 
+	while(1){
+		  if(Firmata.available() > 0){
+		     Firmata.processInput();
+		  }
+		  else{
 
-	  while (Firmata.available())
-		  Firmata.processInput();
+		  }
+//		  Serial.write(i);
+////		  Serial.write(analogRead(5));
+//		  Serial.write(nl,2);
+//		  delay(1000);
+	  }
 
 //  while (1)
 //  {
