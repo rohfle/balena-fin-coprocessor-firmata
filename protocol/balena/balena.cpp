b7f6f140007c67dfd5a5afa3089ce644b2a595ba
Alex Bucknall <alex.bucknall@gmail.com>
2019-02-07 11:02:18 +0000

    WIP Tidied up balena lib

--- protocol/balena/balena.cpp
+++ protocol/balena/balena.cpp
@@ -1,43 +1,59 @@
 #include "balena.h"
 
-/* Balena Functions */
+/******************************************************************************
+ * @brief  Balena Definitions
+ *
+ *****************************************************************************/
 
-/* Analog Variables */
+/* ADC */
 ADC_Init_TypeDef ADCInit;
 ADC_InitSingle_TypeDef ADCSingle;
 ADC_PosSel_TypeDef * ADCNone;
-
 bool ADCset = false;
 
+/* IDAC */
 IDAC_Init_TypeDef DACInit = IDAC_INIT_DEFAULT;
 IDAC_OutMode_TypeDef * IDACNone;
 
 /* RTC */
 RTCDRV_TimerID_t id;
 
-/* Pin Definitions */
+/******************************************************************************
+ * @brief  Pin Definitions
+ *
+ *****************************************************************************/
+
+/* User Accessible Pins */
 PIN_MAP port_pin[NUM_PINS] = {
 	{gpioPortD, 14, adcPosSelAPORT3XCH6, idacOutputAPORT1YCH9, MODE_NONE},   // P0
-	{gpioPortB, 13, adcPosSelAPORT3YCH29, idacOutputAPORT1YCH29, MODE_NONE}, // P1
+	{gpioPortB, 13, adcPosSelAPORT3YCH29, idacOutputAPORT1YCH29, MODE_NONE}, // P1 (SPI CS)
 	{gpioPortA, 2, adcPosSelAPORT4YCH10, idacOutputAPORT1XCH10, MODE_NONE},  // P2
-	{gpioPortC, 8, adcPosSelAPORT2YCH8,  * IDACNone, MODE_NONE},             // P3
+	{gpioPortC, 8, adcPosSelAPORT2YCH8,  * IDACNone, MODE_NONE},             // P3 (SPI CLK)
 	{gpioPortA, 3, adcPosSelAPORT3YCH11, idacOutputAPORT1YCH11, MODE_NONE},  // P4
-	{gpioPortC, 6, adcPosSelAPORT1XCH6, * IDACNone, MODE_NONE},              // P5
+	{gpioPortC, 6, adcPosSelAPORT1XCH6, * IDACNone, MODE_NONE},              // P5 (SPI MOSI)
 	{gpioPortA, 4, adcPosSelAPORT4YCH12, idacOutputAPORT1XCH12, MODE_NONE},  // P6
-	{gpioPortC, 7, adcPosSelAPORT2XCH7, * IDACNone, MODE_NONE},              // P7
+	{gpioPortC, 7, adcPosSelAPORT2XCH7, * IDACNone, MODE_NONE},              // P7 (SPI MISO)
 	{gpioPortA, 5, adcPosSelAPORT3YCH13, idacOutputAPORT1YCH13, MODE_NONE},  // P8
 	{gpioPortA, 1, adcPosSelAPORT3YCH9, idacOutputAPORT1YCH9, MODE_NONE},    // P9
-	{gpioPortB, 11, adcPosSelAPORT3YCH27, idacOutputAPORT1YCH9, MODE_NONE},  // P10 <- Test ADC
+	{gpioPortB, 11, adcPosSelAPORT3YCH27, idacOutputAPORT1YCH9, MODE_NONE},  // P10
 	{gpioPortA, 0, adcPosSelAPORT3XCH8, idacOutputAPORT1XCH8, MODE_NONE},    // P11
 	{gpioPortF, 6, adcPosSelAPORT2YCH22, * IDACNone, MODE_NONE},             // P12 (Dev Kit LED0)
 	{gpioPortD, 15, adcPosSelAPORT3YCH7, idacOutputAPORT1YCH7, MODE_NONE},   // P13
 	{gpioPortF, 7, adcPosSelAPORT2XCH23, * IDACNone, MODE_NONE},             // P14 (Dev Kit LED1)
 	{gpioPortD, 13, adcPosSelAPORT3YCH5, idacOutputAPORT1YCH5, MODE_NONE},   // P15
-	{gpioPortF, 5, * ADCNone, * IDACNone, MODE_NONE},                        // P16 (Placeholder ADC)
-	{gpioPortC, 9, * ADCNone, * IDACNone, MODE_NONE},                        // P17 (Placeholder ADC)
-	{gpioPortC, 10, * ADCNone, * IDACNone, MODE_NONE},                       // P18 (Placeholder ADC)
-	{gpioPortC, 11, * ADCNone, * IDACNone, MODE_NONE}                        // P19 (Placeholder ADC)
+	{gpioPortF, 5, * ADCNone, * IDACNone, MODE_NONE},                        // P16 (PW_ON_3V3)
+	{gpioPortC, 9, * ADCNone, * IDACNone, MODE_NONE},                        // P17 (PW_ON_5V)
+	{gpioPortC, 10, * ADCNone, * IDACNone, MODE_NONE},                       // P18 (I2C SDA)
+	{gpioPortC, 11, * ADCNone, * IDACNone, MODE_NONE}                        // P19 (I2C SCL)
 };
+
+/* Reserved Pins */
+
+
+/******************************************************************************
+ * @brief  Balena Functions
+ *
+ *****************************************************************************/
 
 void balenaInit()
 {
@@ -47,20 +63,22 @@ void balenaInit()
 	initADC();
 }
 
+/******************************************************************************
+ * @brief  System Functions
+ *
+ *****************************************************************************/
+
 void initMCU(){
 	// Device errata
 	CHIP_Init();
-
 	// Set up DC-DC converter
 	EMU_DCDCInit_TypeDef dcdcInit = BSP_DCDC_INIT;
 	#if HAL_DCDC_BYPASS
 	dcdcInit.dcdcMode = emuDcdcMode_Bypass;
 	#endif
 	EMU_DCDCInit(&dcdcInit);
-
 	// Set up clocks
 	initMCU_CLK();
-
 	RTCC_Init_TypeDef rtccInit = RTCC_INIT_DEFAULT;
 	rtccInit.enable                = true;
 	rtccInit.debugRun              = false;
@@ -96,11 +114,6 @@ void initMCU_CLK(void)
   if (0==(DEVINFO->MODULEINFO & DEVINFO_MODULEINFO_HFXOCALVAL_MASK)) {
      hfxoInit.ctuneSteadyState = DEVINFO_MODULEINFO_CRYSTALOSCCALVAL & DEVINFO_HFXOCTUNE_MASK;
   }
-#if defined(BSP_CLK_HFXO_CTUNE) && BSP_CLK_HFXO_CTUNE > -1
-  else {
-    hfxoInit.ctuneSteadyState = BSP_CLK_HFXO_CTUNE;
-  }
-#endif
   CMU_HFXOInit(&hfxoInit);
 
   // Set system HFXO frequency
@@ -139,35 +152,21 @@ void initMCU_CLK(void)
   CMU_ClockSelectSet(cmuClock_LFE, cmuSelect_LFXO);
 }
 
-void initGPIO(){
-	CMU_ClockEnable(cmuClock_GPIO, true);
-	EMU_DCDCInit_TypeDef dcdcInit = EMU_DCDCINIT_DEFAULT;
-	EMU_DCDCInit(&dcdcInit);
-};
 
-void initTimer(){
-	USTIMER_Init();
-	// Initialization of RTCDRV driver
-	RTCDRV_Init();
 
-	// Reserve a timer
-	RTCDRV_AllocateTimer( &id );
-};
-
-uint32_t millis(){
-	return(RTCDRV_GetWallClockTicks32()/4);
-};
-
+/******************************************************************************
+ * @brief  ADC Functions
+ *
+ *****************************************************************************/
 
 void initADC(){
 	CMU_ClockEnable(cmuClock_ADC0, true);
 	ADCInit = ADC_INIT_DEFAULT;
 	ADCSingle = ADC_INITSINGLE_DEFAULT;
-	ADCInit.prescale = ADC_PrescaleCalc(adcFreq, 0); // Init to max ADC clock for Series 0
-
-	ADCSingle.diff       = false;        // single ended
-	ADCSingle.reference  = adcRef5V;    // internal 2.5V reference
-	ADCSingle.resolution = adcRes12Bit;  // 12-bit resolution
+	ADCInit.prescale = ADC_PrescaleCalc(adcFreq, 0);
+	ADCSingle.diff       = false;           // single ended
+	ADCSingle.reference  = adcRef5V;        // internal 2.5V reference
+	ADCSingle.resolution = adcRes12Bit;     // 12-bit resolution
 	ADCSingle.acqTime    = adcAcqTime4;     // set acquisition time to meet minimum requirement
 };
 
@@ -177,6 +176,11 @@ void setADC(unsigned int pin_no, ADC_TypeDef * adc){
 	ADC_Init(adc, &ADCInit);
 	ADC_InitSingle(adc, &ADCSingle);
 };
+
+/******************************************************************************
+ * @brief  IDAC Functions
+ *
+ *****************************************************************************/
 
 void initIDAC(){
 	CMU_ClockEnable(cmuClock_IDAC0, true);
@@ -201,7 +205,10 @@ void setIDAC(unsigned int pin_no, uint8_t step){
 	IDAC_OutEnable(IDAC0, true);
 };
 
-/* Arduino Functions */
+/******************************************************************************
+ * @brief  Arduino-Firmata Functions
+ *
+ *****************************************************************************/
 
 void deviceMode(unsigned int pin, unsigned int mode){
 	GPIO_PinModeSet(BSP_VCOM_ENABLE_PORT, BSP_VCOM_ENABLE_PIN, gpioModePushPull, 1);
@@ -220,7 +227,16 @@ bool pinExists(unsigned int pin_no){
 	};
 };
 
-/* Digital I/O */
+/******************************************************************************
+ * @brief  Digital I/O Functions
+ *
+ *****************************************************************************/
+
+void initGPIO(){
+	CMU_ClockEnable(cmuClock_GPIO, true);
+	EMU_DCDCInit_TypeDef dcdcInit = EMU_DCDCINIT_DEFAULT;
+	EMU_DCDCInit(&dcdcInit);
+};
 
 unsigned int digitalRead(unsigned int pin_no){
 	return GPIO_PinInGet(port_pin[pin_no].port, port_pin[pin_no].pin);
@@ -230,31 +246,43 @@ void digitalWrite(unsigned int pin_no, unsigned int value){
 	GPIO_PinModeSet(port_pin[pin_no].port, port_pin[pin_no].pin , gpioModePushPull, value);
 };
 
-/* Analog I/O */
+/******************************************************************************
+ * @brief  Analogue I/O Functions
+ *
+ *****************************************************************************/
 
 void analogWrite(unsigned int pin_no, byte value){
 	if(port_pin[pin_no].dac != * IDACNone){
 		if(port_pin[pin_no].state == MODE_ANALOG_OUT){
 			// setup IDAC
 		}
-		else {
-			port_pin[pin_no].state = MODE_ANALOG_OUT;
-
-		};
+		else port_pin[pin_no].state = MODE_ANALOG_OUT;
 	}
 };
 
 uint32_t analogRead(unsigned int pin_no){
 	if(port_pin[pin_no].state != MODE_ANALOG_IN){
 		setADC(pin_no, ADC0);
-//		digitalWrite(3,0);
 	}
 	 ADC_Start(ADC0, adcStartSingle);
 	 while(!(ADC0->STATUS & _ADC_STATUS_SINGLEDV_MASK));
 	return  ADC_DataSingleGet(ADC0);
 };
 
-/* Timer Functions */
+/******************************************************************************
+ * @brief  Timer/RTC Functions
+ *
+ *****************************************************************************/
+
+void initTimer(){
+	USTIMER_Init();
+	RTCDRV_Init();
+	RTCDRV_AllocateTimer( &id );
+};
+
+uint32_t millis(){
+	return(RTCDRV_GetWallClockTicks32()/4);
+};
 
 void delay(unsigned int n){
 	USTIMER_Delay(n * 1000);
